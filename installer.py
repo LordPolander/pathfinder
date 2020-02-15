93e553a804c35b013d8f259f5cdd708d5071e972
imifk <ignacykurowski@gmail.com>
2020-02-15 01:32:40 +0000

    installer-deletes-old-files

--- installer.py
+++ installer.py
@@ -4,14 +4,6 @@ import os
 # install install.txt
 # txt file containing list of files
 
-url = 'https://github.com/imifk/pathfinder/blob/master/install.txt?raw=true'
-
-f = urllib.request.urlopen(url)
-file = f.read()
-f.close()
-f2 = open('install.txt', 'wb')
-f2.write(file)
-f2.close()
 
 
 def find_files(file):
@@ -20,9 +12,6 @@ def find_files(file):
     # you may also want to remove whitespace characters like `\n` at the end of each line
     return [x.strip() for x in content]
 
-
-contents = find_files('install.txt')
-print(contents)
 
 
 def install():
@@ -37,17 +26,46 @@ def install():
         f2.close()
     return
 
+def reinstall():
+    try:
+        f = open("install.txt")
+        contents = find_files('install.txt')
+        print('deleting old files . . .')
+        print(contents)
+        for content in contents:
+            print(content)
+            os.remove(content)
+        f.close()
+        os.remove('install.txt')
+    except IOError:
+        print("File not accessible")
+    finally:
+        pass
+
 def hide_file():
+    os.system(f'attrib +h install.txt')
     for content in contents:
         os.system(f'attrib +h {content}')
     os.system('attrib -h Pathinator.exe')
     return
 
+reinstall()
+
+url = 'https://github.com/imifk/pathfinder/blob/master/install.txt?raw=true'
+
+f = urllib.request.urlopen(url)
+file = f.read()
+f.close()
+f2 = open('install.txt', 'wb')
+f2.write(file)
+f2.close()
+
+contents = find_files('install.txt')
+print(f'downloading {contents}')
+
 install()
+
 hide_file()
-
-
-os.remove('install.txt')
 
 print('finished             ')
 print('if install.txt is present.')
